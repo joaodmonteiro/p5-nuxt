198a87250cfc1934c5a5f8ce7122cbe8313ebb02
joaodmonteiro <joaodmonteiro10@gmail.com>
2022-12-01 13:43:52 +0000

    commit

--- js/p5.js
+++ js/p5.js
@@ -1,451 +1,130 @@
-import { getNoiseColor, setGradient } from './noiseColor'
-
 let p5
-let color
-let size
-let pencil
-let noise
-let xoff = 0
-let yoff = 0
 let fps
-let diyPencilPositions = {}
-let brushSize = 50
-let stroke
+let text
+let textLayer
+let gridLayer
+let newLayer
+let tileSize = 50
+let tiles = {}
+let offsetX = 0
+let offsetY = 0
+let xOff = 0
+let yOff = 0
 
 export function main(_p5) {
   p5 = _p5
-
-  const squareWidth = 10
-  const squares = []
-
-  for (let i = 0; i < 60; i++) {
-    for (let j = 0; j < 60; j++) {
-      squares.push([i * squareWidth, j * squareWidth])
-    }
-  }
+  p5.disableFriendlyErrors = true
 
   // NOTE: Set up is here
   p5.setup = (_) => {
-    const canvas = p5.createCanvas(600, 600)
+    const canvas = p5.createCanvas(800, 600)
     canvas.parent('container')
     fps = p5.createP('')
+
+    textLayer = p5.createGraphics(400, 200)
+    gridLayer = p5.createGraphics(400, 200)
+    newLayer = p5.createGraphics(800, 400)
   }
+
   // NOTE: Draw is here
   p5.draw = (_) => {
     fps.html('FPS: ' + p5.floor(p5.frameRate()))
+    p5.clear()
 
-    if (p5.frameCount === 1) {
-      squares.forEach((sq) => {
-        p5.stroke(200)
-        p5.fill(255)
-        p5.square(sq[0], sq[1], squareWidth)
-      })
-    }
-
-    let square = {}
-    const heart = {}
-    const smile = {}
-    const checkers = {}
-    const diagonal = {}
-    const houndstooth = {}
-    const diy = {}
-    const circle = {}
-    const triangle = {}
-
-    if (p5.mouseIsPressed) {
-      if (0 < p5.mouseX < 600 && 0 < p5.mouseY < 600) {
-        switch (pencil) {
-          case 'square':
-            square.x = roundNearest(p5.mouseX, size)
-            square.y = roundNearest(p5.mouseY, size)
-            square.color = color
-            square.size = size
-            break
-          case 'heart':
-            heart.x = roundNearest(p5.mouseX, 50)
-            heart.y = roundNearest(p5.mouseY, 50)
-            heart.color = color
-            break
-          case 'smile':
-            smile.x = roundNearest(p5.mouseX, 50)
-            smile.y = roundNearest(p5.mouseY, 60)
-            smile.color = color
-            break
-          case 'checkers':
-            checkers.x = roundNearest(p5.mouseX, size)
-            checkers.y = roundNearest(p5.mouseY, size)
-            checkers.color = color
-            checkers.size = size
-            break
-          case 'diagonal':
-            diagonal.x = roundNearest(p5.mouseX, size)
-            diagonal.y = roundNearest(p5.mouseY, size)
-            diagonal.color = color
-            diagonal.size = size
-            break
-          case 'houndstooth':
-            houndstooth.x = roundNearest(p5.mouseX, 60)
-            houndstooth.y = roundNearest(p5.mouseY, 60)
-            houndstooth.color = color
-            // houndstooth.size = size
-            break
-          case 'circle':
-            circle.x = roundNearest(p5.mouseX, size)
-            circle.y = roundNearest(p5.mouseY, size)
-            circle.color = color
-            circle.size = size
-            break
-          case 'triangle':
-            triangle.x = roundNearest(p5.mouseX, size)
-            triangle.y = roundNearest(p5.mouseY, size)
-            triangle.color = color
-            triangle.size = size
-            break
-          case 'diy':
-            diy.x = roundNearest(p5.mouseX, brushSize)
-            diy.y = roundNearest(p5.mouseY, brushSize)
-            diy.color = color
-            break
-        }
+    for (let i = 0; i < 200 - tileSize; i++) {
+      for (let j = 0; j < 400 - tileSize; j++) {
+        gridLayer.square(i * tileSize, j * tileSize, tileSize)
       }
     }
-    if (Object.keys(square).length !== 0) {
-      paintSquare(square)
-      square = {}
-    }
 
-    if (Object.keys(checkers).length !== 0) {
-      paintCheckers(checkers)
-    }
+    textLayer.clear()
+    textLayer.textSize(50)
+    textLayer.fill(0)
+    textLayer.text(text, 0, 0, 400, 400)
 
-    if (Object.keys(diagonal).length !== 0) {
-      paintDiagonal(diagonal)
-    }
+    drawTiles(p5)
 
-    if (Object.keys(heart).length !== 0) {
-      paintHeart(heart)
-    }
-
-    if (Object.keys(smile).length !== 0) {
-      paintSmile(smile)
-    }
-
-    if (Object.keys(houndstooth).length !== 0) {
-      paintHoundstooth(houndstooth)
-    }
-
-    if (Object.keys(circle).length !== 0) {
-      paintCircle(circle)
-    }
-
-    if (Object.keys(triangle).length !== 0) {
-      paintTriangle(triangle)
-      console.log(triangle)
-    }
-
-    if (Object.keys(diy).length !== 0) {
-      paintDiy(diy)
-    }
+    p5.image(gridLayer, 0, 0)
+    p5.image(textLayer, 0, 0)
+    p5.image(newLayer, 0, 200)
+    // p5.image(cursorLayer, 0, 0)
   }
+}
+
+export function setText(_text) {
+  text = _text
+}
+
+export function setOffsetX(_offsetX) {
+  offsetX = _offsetX
+}
+
+export function setOffsetY(_offsetY) {
+  offsetY = _offsetY
+}
+
+export function setTileSize(_tileSize) {
+  tileSize = _tileSize
 }
 
 const roundNearest = (value, nearest) => Math.floor(value / nearest) * nearest
 
-export function setColor(_color) {
-  color = _color
-}
-
-export function setSize(_size) {
-  size = parseInt(_size)
-}
-
-export function setStroke(_stroke) {
-  stroke = _stroke
-}
-
-export function setPencil(_pencil) {
-  pencil = _pencil
-}
-
-export function setNoise(_noise) {
-  noise = _noise
-}
-
-export function setDiyPositions(_diyPencilPositions) {
-  diyPencilPositions = _diyPencilPositions
-}
-
-export function setBrushSize(_brushSize) {
-  brushSize = _brushSize * 10
-}
-
-function paintHeart(heart) {
-  const heartPositions = [
-    [0, 1],
-    [0, 2],
-    [1, 0],
-    [1, 1],
-    [1, 2],
-    [1, 3],
-    [2, 1],
-    [2, 2],
-    [2, 3],
-    [2, 4],
-    [3, 0],
-    [3, 1],
-    [3, 2],
-    [3, 3],
-    [4, 1],
-    [4, 2],
-  ]
-  heartPositions.forEach((position) => {
-    if (!stroke) {
-      p5.noStroke()
-    } else {
-      p5.stroke(0)
-    }
-    if (noise) {
-      p5.fill(
-        getNoiseColor(
-          p5,
-          color,
-          heart.x + position[0] * 10,
-          heart.y + position[1] * 10
+export function drawTiles() {
+  newLayer.clear()
+  // newLayer.background(255)
+  Object.keys(tiles).forEach((x) => {
+    Object.keys(tiles[x]).forEach((y) => {
+      if (tiles[x][y]) {
+        newLayer.image(
+          tiles[x][y],
+          parseInt(x),
+          parseInt(y),
+          tileSize,
+          tileSize
         )
-      )
-    } else {
-      p5.fill(heart.color)
-    }
-    p5.square(heart.x + position[0] * 10, heart.y + position[1] * 10, 10)
-  })
-}
-
-function paintSquare(square) {
-  if (!stroke) {
-    p5.noStroke()
-  } else {
-    p5.stroke(0)
-  }
-  p5.fill(square.color)
-  p5.square(square.x, square.y, square.size)
-}
-
-function paintSmile(smile) {
-  const smilePositions = [
-    [0, 0],
-    [0, 1],
-    [0, 2],
-    [0, 3],
-    [0, 4],
-    [0, 5],
-    [1, 1],
-    [1, 3],
-    [1, 5],
-    [2, 1],
-    [2, 2],
-    [2, 3],
-    [2, 5],
-    [3, 1],
-    [3, 3],
-    [3, 5],
-    [4, 0],
-    [4, 1],
-    [4, 2],
-    [4, 3],
-    [4, 4],
-    [4, 5],
-  ]
-
-  smilePositions.forEach((position) => {
-    if (!stroke) {
-      p5.noStroke()
-    } else {
-      p5.stroke(0)
-    }
-    p5.fill(smile.color)
-    p5.square(smile.x + position[0] * 10, smile.y + position[1] * 10, 10)
-  })
-}
-
-function paintCheckers(checkers) {
-  let on = true
-
-  let xoff = 0
-  for (let i = 0; i < checkers.size / 10; i++) {
-    let yoff = 0
-    for (let j = 0; j < checkers.size / 10; j++) {
-      if (on) {
-        if (!stroke) {
-          p5.noStroke()
-        } else {
-          p5.stroke(0)
-        }
-        if (noise) {
-          p5.fill(
-            getNoiseColor(p5, color, checkers.x + i * 10, checkers.y + j * 10)
-          )
-        } else {
-          p5.fill(color)
-        }
-        p5.square(checkers.x + i * 10, checkers.y + j * 10, 10)
-      }
-      if (j !== 9) {
-        on = !on
-      }
-      yoff += 0.01
-    }
-    xoff += 0.01
-  }
-}
-
-function paintDiagonal(diagonal) {
-  let on = true
-  for (let i = 0; i < diagonal.size / 10; i++) {
-    for (let j = 0; j < diagonal.size / 10; j++) {
-      if (i === j) {
-        if (!stroke) {
-          p5.noStroke()
-        } else {
-          p5.stroke(0)
-        }
-        if (noise) {
-          p5.fill(
-            getNoiseColor(
-              p5,
-              diagonal.color,
-              diagonal.x + i * 10,
-              diagonal.y + j * 10
-            )
-          )
-        } else {
-          p5.fill(diagonal.color)
-        }
-        p5.square(diagonal.x + i * 10, diagonal.y + j * 10, 10)
-      }
-    }
-  }
-}
-
-function paintHoundstooth(houndstooth) {
-  const houndstoothPositions = [
-    [0, 0],
-    [0, 1],
-    [0, 2],
-    [0, 3],
-    [1, 0],
-    [1, 1],
-    [1, 2],
-    [1, 4],
-    [2, 0],
-    [2, 1],
-    [2, 2],
-    [2, 3],
-    [2, 5],
-    [3, 0],
-    [3, 2],
-    [4, 1],
-    [5, 2],
-  ]
-
-  houndstoothPositions.forEach((position) => {
-    if (!stroke) {
-      p5.noStroke()
-    } else {
-      p5.stroke(0)
-    }
-    if (noise) {
-      p5.fill(
-        getNoiseColor(p5, houndstooth.color, houndstooth.x, houndstooth.y)
-      )
-    } else {
-      p5.fill(houndstooth.color)
-    }
-    p5.square(
-      houndstooth.x + position[0] * 10,
-      houndstooth.y + position[1] * 10,
-      10
-    )
-  })
-}
-
-function paintCircle(circle) {
-  if (!stroke) {
-    p5.noStroke()
-  } else {
-    p5.stroke(0)
-  }
-  p5.fill(circle.color)
-  p5.ellipse(
-    circle.x + circle.size / 2,
-    circle.y + circle.size / 2,
-    circle.size,
-    circle.size
-  )
-}
-
-function paintTriangle(triangle) {
-  if (!stroke) {
-    p5.noStroke()
-  } else {
-    p5.stroke(0)
-  }
-  p5.fill(triangle.color)
-  p5.triangle(
-    triangle.x,
-    triangle.y + triangle.size,
-    triangle.x + triangle.size / 2,
-    triangle.y,
-    triangle.x + triangle.size,
-    triangle.y + triangle.size
-  )
-
-  console.log(
-    triangle.x,
-    triangle.y + triangle.size,
-    triangle.x + triangle.size / 2,
-    triangle.y,
-    triangle.x + triangle.size,
-    triangle.y + triangle.size
-  )
-}
-
-function paintDiy(diy) {
-  if (Object.keys(diyPencilPositions).length === 0) return
-
-  Object.keys(diyPencilPositions).forEach((y) => {
-    if (Object.keys(diyPencilPositions[y]).length === 0) return
-    Object.keys(diyPencilPositions[y]).forEach((x) => {
-      if (diyPencilPositions[y][x] === true) {
-        if (!stroke) {
-          p5.noStroke()
-        } else {
-          p5.stroke(0)
-        }
-        if (noise) {
-          p5.fill(getNoiseColor(p5, diy.color, diy.x + x * 10, diy.y + y * 10))
-        } else {
-          p5.fill(diy.color)
-        }
-
-        p5.square(diy.x + x * 10, diy.y + y * 10, 10)
+        // xOff += 0.0001
+        // yOff += 0.0001
       }
     })
   })
 }
 
-export function clearCanvas() {
-  const squareWidth = 10
-  const squares = []
+export function copyTiles() {
+  const emptyObj = {}
+  tiles = emptyObj
+  for (let i = 0; i < 400 / tileSize; i++) {
+    for (let j = 0; j < 800 / tileSize; j++) {
+      let tileImage = p5.createImage(tileSize, tileSize)
+      tileImage.copy(
+        textLayer,
+        (Math.floor(j / 2) + 1) * tileSize - offsetX * j,
+        (Math.floor(i / 2) + 1) * tileSize - offsetY * i,
+        tileSize,
+        tileSize,
+        0,
+        0,
+        tileSize,
+        tileSize
+      )
 
-  for (let i = 0; i < 60; i++) {
-    for (let j = 0; j < 60; j++) {
-      squares.push([i * squareWidth, j * squareWidth])
+      const x = j * tileSize
+      const y = i * tileSize
+
+      if (tiles[x]) {
+        if (tiles[x][y]) {
+          tiles[x][y] = tileImage
+        } else {
+          tiles[x] = {
+            ...tiles[x],
+            [y]: tileImage,
+          }
+        }
+      } else {
+        tiles = {
+          ...tiles,
+          [x]: { [y]: tileImage },
+        }
+      }
     }
   }
-
-  squares.forEach((sq) => {
-    p5.stroke(200)
-    p5.fill(255)
-    p5.square(sq[0], sq[1], squareWidth)
-  })
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,53 +1,13 @@
 <template>
-  <main :style="computedStyles">
+  <main>
     <div id="container"></div>
-    <select name="pencil" id="pencil" v-model="pencil">
-      <option value="square">Square</option>
-      <option value="circle">Circle</option>
-      <option value="triangle">Triangle</option>
-      <option value="checkers">Checkers</option>
-      <option value="diagonal">Diagonal</option>
-      <option value="heart">Heart</option>
-      <option value="smile">Batman</option>
-      <option value="houndstooth">Houndstooth</option>
-      <option value="diy">Custom brush</option>
-    </select>
-    <input v-if="!noise" type="color" id="color" v-model="color">
-    <select v-else name="color" id="color" v-model="color">
-      <option value="#000000">Black</option>
-      <!-- <option value="#FFFFFF">White</option> -->
-      <option value="#F50000">Red</option>
-      <option value="#3CC700">Green</option>
-      <option value="#0030C7">Blue</option>
-    </select>
-    <select
-      v-if="pencil === 'square' || pencil === 'checkers' || pencil === 'diagonal' || pencil === 'circle' || pencil === 'triangle'"
-      name="size" id="size" v-model="size">
-      <option value="10">10px</option>
-      <option value="50">50px</option>
-      <option value="100">100px</option>
-    </select>
-    <input type="checkbox" name="stroke" value="stroke" id="stroke" v-model="stroke">
-    <label for="stroke">Stroke</label>
-    <button @click="clearCanvas()">Clear canvas!</button>
-    <div v-if="pencil !== 'square'">
-      <input type="checkbox" name="Perlin Noise" id="noise" value="noise" v-model="noise">
-      <label for="noise">Perlin Noise</label>
-    </div>
-
-    <div class="pencil-maker">
-      <div class="pencil-maker__row" v-for="y in parseInt(brushSize)">
-        <div class="pencil-maker__item" v-for="x in parseInt(brushSize)"
-          :class="{ selected: diyPencilPositions[x - 1] && diyPencilPositions[x - 1][y - 1] === true }"
-          @mouseover="mouseDown && toggleSquare(x - 1, y - 1)" @mousedown="toggleSquare(x - 1, y - 1)">
-        </div>
-      </div>
-    </div>
-    <div class="brush-size">
-      <input class="brush-size__slider" type="range" min="5" max="20" value="5" id="brushSize" v-model="brushSize">
-    </div>
-    <label for="brushSize">{{ brushSize }}</label>
-    <button @click="clearBrush()">Clear Brush</button>
+    <input type="text" v-model="text">
+    <label for="offsetX">Offset X</label>
+    <input type="range" min="0" :max="tileSize" value="0" v-model="offsetX">
+    <label for="offsetY">Offset Y</label>
+    <input type="range" min="0" :max="tileSize" value="0" v-model="offsetY">
+    <label for="tileSize">Tile Size</label>
+    <input type="range" min="10" max="100" value="50" step="5" v-model="tileSize">
   </main>
 </template>
 
@@ -57,124 +17,48 @@ export default {
   name: 'IndexPage',
   data() {
     return {
-      color: '#000000',
-      size: 10,
-      pencil: 'square',
-      noise: false,
-      diyPencilPositions: {},
-      brushSize: 5,
-      mouseDown: false,
-      stroke: false
+      text: '',
+      offsetX: 0,
+      offsetY: 0,
+      tileSize: 50
     }
   },
   computed: {
-    computedStyles() {
-      return {
-        '--brush-size': parseInt(this.brushSize)
-      }
-    }
+
   },
   mounted() {
     const P5 = require('p5')
     new P5(canvas.main)
 
-    canvas.setColor(this.color)
-    canvas.setSize(this.size)
-    canvas.setPencil(this.pencil)
-
-    document.addEventListener('mousedown', () => this.mouseDown = true)
-    document.addEventListener('mouseup', () => this.mouseDown = false)
   },
   beforeDestroy() {
-    document.removeEventListener('mousedown', () => this.mouseDown = true)
-    document.removeEventListener('mouseup', () => this.mouseDown = false)
   },
   watch: {
-    size() {
-      canvas.setSize(this.size)
+    text() {
+      canvas.setText(this.text)
+      canvas.copyTiles()
     },
-    stroke() {
-      canvas.setStroke(this.stroke)
+    offsetX() {
+      canvas.setOffsetX(this.offsetX)
+      canvas.copyTiles()
     },
-    color() {
-      canvas.setColor(this.color)
+    offsetY() {
+      canvas.setOffsetY(this.offsetY)
+      canvas.copyTiles()
     },
-    pencil() {
-      canvas.setPencil(this.pencil)
-    },
-    noise() {
-      canvas.setNoise(this.noise)
-    },
-    diyPencilPositions: {
-      handler() {
-        canvas.setDiyPositions(this.diyPencilPositions)
-        console.log(this.diyPencilPositions)
-      },
-      deep: true
-    },
-    brushSize() {
-      canvas.setBrushSize(this.brushSize)
-
-      Object.keys(this.diyPencilPositions).forEach(x => {
-        Object.keys(this.diyPencilPositions).forEach(y => {
-          if (y >= this.brushSize) {
-            delete this.diyPencilPositions[x][y]
-          }
-        })
-        if (x >= this.brushSize) {
-          delete this.diyPencilPositions[x]
-        }
-      })
+    tileSize() {
+      canvas.setTileSize(this.tileSize)
+      canvas.copyTiles()
     }
   },
   methods: {
-    toggleSquare(x, y) {
-      const newObj = { ...this.diyPencilPositions }
-      if (newObj[x]) {
-        if (newObj[x][y]) {
-          newObj[x][y] = !newObj[x][y]
-        } else {
-          newObj[x][y] = true
-        }
-      } else {
-        newObj[x] = {}
-        newObj[x][y] = true
-      }
 
-      this.diyPencilPositions = newObj
-      console.log(this.diyPencilPositions)
-    },
-    clearBrush() {
-      const emptyObj = {}
-
-      this.diyPencilPositions = emptyObj
-    },
-    clearCanvas() {
-      canvas.clearCanvas()
-    }
   }
 }
 </script>
 
 <style>
-.pencil-maker {
-  display: grid;
-  grid-template-columns: repeat(var(--brush-size), 10px);
-  margin-top: 20px;
-}
-
-.pencil-maker__item {
-  width: 10px;
-  height: 10px;
-  border: 1px solid gray;
-  box-sizing: border-box;
-}
-
-.selected {
-  background-color: black;
-}
-
-#brushSize {
-  width: 100px;
+#container {
+  border: 1px solid black;
 }
 </style>
